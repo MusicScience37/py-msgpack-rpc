5def30a62c5521ef74a41eea83050336b5d41ef4
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-09-14 15:34:53 +0000

    :sparkles: Implementation of servers.

--- py_msgpack_rpc/_server/_method_executor.py
+++ py_msgpack_rpc/_server/_method_executor.py
@@ -1,8 +1,11 @@
 """Implementation of MethodExecutor."""
 
+import logging
 import typing
 
 from py_msgpack_rpc._messages import Request, Response
+
+LOGGER = logging.getLogger(__name__)
 
 
 class MethodExecutor:
@@ -18,6 +21,7 @@ class MethodExecutor:
             method_name (str): Method name.
             method_function (typing.Callable): Function of the method.
         """
+        LOGGER.debug('Register a method "%s"', method_name)
         self._methods[method_name] = method_function
 
     async def process_request(self, request: Request) -> Response:
@@ -30,6 +34,7 @@ class MethodExecutor:
             Response: Response.
         """
         try:
+            LOGGER.debug("Request %s (id: %s)", request.method_name, request.message_id)
             result = self._methods[request.method_name](*(request.parameters))
             return Response(
                 message_id=request.message_id,
@@ -37,6 +42,12 @@ class MethodExecutor:
                 result=result,
             )
         except Exception as exception:  # pylint: disable=broad-exception-caught
+            LOGGER.debug(
+                "Error in request %s (id: %s)",
+                request.method_name,
+                request.message_id,
+                exc_info=True,
+            )
             return Response(
                 message_id=request.message_id,
                 error=exception.args,
